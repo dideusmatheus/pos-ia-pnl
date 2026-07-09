05dc06607487a4232c888fea969e076735ddeaac
Matheus <matheus_deusr@hotmail.com>
2026-07-09 15:56:12 -0300

    Cotagem de avaliacoes positivas e negativas

--- main.py
+++ main.py
@@ -3,14 +3,24 @@ import pandas as pd
 
 # carregar o arquivo CSV com as avaliações
 avaliacoes = pd.read_csv("data/Brazilian-Portuguese-Sentiment-Analysis-Datasets.csv")
+print('\n\n')
+print("Arquivo CSV carregado com sucesso!")
 print(avaliacoes.head())
+print('\n\n')
 
 # remover colunas desnecessárias
 avaliacoes = avaliacoes.drop(["original_index", "review_text_processed", "review_text_tokenized", "rating", "kfold_polarity", "kfold_rating"], axis=1)
+print("Colunas desnecessárias removidas com sucesso!")
 print(avaliacoes.head())
+print('\n\n')
 
 # remover linhas com valores nulos
 avaliacoes.dropna(inplace=True, axis=0)
+print("Linhas com valores nulos removidas com sucesso!")
 print(avaliacoes.head())
+print('\n\n')
 
-
+# contar a quantidade de avaliações positivas, negativas e neutras
+avaliacoes.polarity.value_counts()
+print("Contagem de avaliações por polaridade:")
+print(avaliacoes.polarity.value_counts())
